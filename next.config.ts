f09396924fe653de40ad86e6248dbb2f6825e980
Mikhail Davydov <davydov@portal.me>
2025-02-23 22:43:53 +0100

    fix: Repo prefix

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  // Required for GitHub Pages compatibility
+  basePath: process.env.NODE_ENV === "production" ? "/der-die-das" : "",
 };
 
 export default nextConfig;
